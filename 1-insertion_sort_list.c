17f5c0562257464c17ec5390223a2873fc933208
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-18 21:13:16 +0100

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * using insertion algorithm
+ * @list: (listint_t) list of doubly linked lists
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+
+}
