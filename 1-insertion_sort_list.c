77e68a82c7fc933253e6363b218813ccf1721429
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-19 18:23:03 +0100

    insertion on dll

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,48 @@
 #include "sort.h"
 
 /**
+ * swapNodes - swap two nodes of doubly linked list
+ * @head: collection of nodes
+ * @node1: first node
+ * @node2: second node
+*/
+
+void swapNodes(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	node1->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*head = node2;
+	node1->prev = node2;
+	node1 = node2->prev;
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list
  * using insertion algorithm
  * @list: (listint_t) list of doubly linked lists
 */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *it, *hole, *tmp;
 
+	if (list == NULL || (*list)->next == NULL || *list == NULL)
+		return;
+
+	for (it = (*list)->next; it != NULL; it = tmp)
+	{
+		tmp = it->next;
+		hole = it->prev;
+		while (hole != NULL && it->n < hole->n)
+		{
+			swapNodes(list, hole, it);
+			print_list((const listint_t *)*list);
+		}
+	}
 
 }
--- compile.sh
+++ compile.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+main1(){
 gcc -Wall -Wextra -Werror -pedantic  -std=gnu89 0-bubble_sort.c 0-main.c print_array.c -o bubble
+}
+main2(){
+    gcc -Wall -Wextra -Werror -pedantic  -std=gnu89 1-main.c 1-insertion_sort_list.c print_list.c -o insertion
+}
+
+if [ $1 -eq 1 ]
+then 
+    main1
+elif [ $1 -eq 2 ]
+then
+    main2
+else
+    echo "invalid option"
+fi
+
+echo "Compiling $1"
