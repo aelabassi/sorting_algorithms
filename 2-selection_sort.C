73e34b1976f04a1b27b1166d8b9c7780d2899b6f
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-20 19:30:48 +0100

    selection sort bug fix

--- 2-selection_sort.C
+++ 2-selection_sort.C
@@ -7,10 +7,28 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    int imin, i, tmp;
+size_t imin, i, j;
+size_t tmp;
 
-    for (i = 0; i < n; i++)
+if (array == NULL)
+{
+    return;
+}
+
+for (i = 0; i < size - 1; i++)
+{
+    imin = i;
+    for (j = i + 1; j < size; j++)
     {
-        
+        if (array[i] < array[imin])
+        {
+            imin = j;
+        }
+        tmp = array[i];
+        array[i] = array[imin];
+        array[imin] = tmp;
+        print_array(array, size);
     }
+}
+
 }
